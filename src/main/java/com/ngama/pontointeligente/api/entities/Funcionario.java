50fa064a952eccacc7900662ed8546f835c301ec
gamantanan <Natanael.n@hotmail.com>
2021-04-13 15:48:19 -0300

    Adicionado Entities e Enums

---
+++ src/main/java/com/ngama/pontointeligente/api/entities/Funcionario.java
@@ -0,0 +1,10 @@
+package com.ngama.pontointeligente.api.entities;
+
+public class Funcionario {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/ngama/pontointeligente/api/entities/Lancamento.java
@@ -0,0 +1,5 @@
+package com.ngama.pontointeligente.api.entities;
+
+public class Lancamento {
+
+}
